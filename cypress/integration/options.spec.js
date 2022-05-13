61e8731a309d1b89de8f1b1eb4a17011b3ce4ecf
tatianasim <79799027+tatianasim@users.noreply.github.com>
2022-05-13 19:04:20 +0300

    update options.spec.js and auth.spec.js (#4)

--- cypress/integration/options.spec.js
+++ cypress/integration/options.spec.js
@@ -1,23 +1,22 @@
 describe('Options', () => {
-  it('Profile form fill', () => {
-
+  before(() => {
     cy.visit('/')
 
     cy.request({
       method: 'POST',
-      url:'https://server-prod.pasv.us/user/login',
+      url:`${Cypress.env('api_server')}/user/login`,
       body: {
-        email: 'test33@example.com',
-        password: 'qwer123Ty'
+        email: Cypress.env('login'),
+        password: Cypress.env('password')
       }
     }).then((response) => {
       window.localStorage.setItem('userId', response.body.payload.userId)
-      window.localStorage.setItem('userId', response.body.payload.token)
+      window.localStorage.setItem('token', response.body.payload.token)
 
 
       cy.request({
         method: 'PATCH',
-        url: 'https://server-prod.pasv.us/user/profile',
+        url: `${Cypress.env('api_server')}/user/profile`,
         headers: {
           Authorization: response.body.payload.token
         },
@@ -34,22 +33,24 @@ describe('Options', () => {
       })
 
       cy.visit(`/settings/${response.body.payload.userId}/profile`)
-
-      cy.get('[data-qa="englishLevel"]')
-        .click()
-      cy.get('[class*="ant-select-item"][title="Intermediate"]')
-        .click()
-
-      cy.intercept('PATCH', '/user/profile')
-        .as('profileChange')
-
-      cy.get('.ant-btn-primary')
-        .click()
-
-      cy.wait('@profileChange')
-        .then((interception)=> {
-          expect(interception.response.body.message).to.eq('User profile updated')
-        })
     })
+  })
+
+  it('Profile form fill', () => {
+    cy.get('[data-qa="englishLevel"]')
+      .click()
+    cy.get('[class*="ant-select-item"][title="Intermediate"]')
+      .click()
+
+    cy.intercept('PATCH', '/user/profile')
+      .as('profileChange')
+
+    cy.get('.ant-btn-primary')
+      .click()
+
+    cy.wait('@profileChange')
+      .then((interception)=> {
+        expect(interception.response.body.message).to.eq('User profile updated')
+      })
   })
 })
