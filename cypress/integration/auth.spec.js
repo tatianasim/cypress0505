66c8bfe1458dfcb6345be4b09595bd9ab60bcf12
Tatiana <s_tvl@mail.ru>
2022-05-13 19:03:34 +0300

    update options.spec.js and auth.spec.js

--- cypress/integration/auth.spec.js
+++ cypress/integration/auth.spec.js
@@ -1,53 +1,53 @@
 describe('Auth', () => {
-    it('Log in with valid credentials', () => {
+  beforeEach(()=> {
+    cy.visit('/user/login')
+  })
 
-        cy.intercept('POST', '/user/login')
-            .as('login')
+  it('Log in with valid credentials', () => {
+    cy.intercept('POST', '/user/login')
+      .as('login')
 
-        cy.visit('/user/login')
-        cy.get('#normal_login_email')
-            .type('test33@example.com')
-        cy.get('#normal_login_password')
-            .type('qwer123Ty')
-        cy.get('.login-form-button')
-            .click()
+    cy.get('#normal_login_email')
+      .type(Cypress.env('login'))
+    cy.get('#normal_login_password')
+      .type(Cypress.env('password'))
+    cy.get('.login-form-button')
+      .click()
 
-        cy.wait('@login')
-            .then(({response})=> {
-                cy.url().should('include', response.body.payload.userId)
-            })
-        cy.get('.ant-avatar-square').should('be.visible')
+    cy.wait('@login')
+      .then(({response})=> {
+        cy.url().should('include', response.body.payload.userId)
+      })
+    cy.get('.ant-avatar-square')
+      .should('be.visible')
 
-    })
+  })
 
-    it('Email input field is required', () => {
-        cy.visit('user/login')
-        cy.get('#normal_login_email')
-            .type('test33@example.com')
-            .clear()
+  it('Email input field is required', () => {
+    cy.get('#normal_login_email')
+      .type('test')
+      .clear()
 
-        cy.xpath('//div[contains(@class, "ant-col")][div//input[@id="normal_login_email"]]//div[@role="alert"]')
-            .should('have.text', 'Required')
-    })
+    cy.xpath('//div[contains(@class, "ant-col")][div//input[@id="normal_login_email"]]//div[@role="alert"]')
+      .should('have.text', 'Required')
+  })
 
-    it('Email incorrect validation', () => {
-        cy.visit('user/login')
-        cy.get('#normal_login_email')
-            .type('test')
+  it('Email incorrect validation', () => {
+    cy.get('#normal_login_email')
+      .type('test')
 
 
-        cy.xpath('//div[contains(@class, "ant-col")][div//input[@id="normal_login_email"]]//div[@role="alert"]')
-            .should('have.text', '\'email\' is not a valid email')
-    })
+    cy.xpath('//div[contains(@class, "ant-col")][div//input[@id="normal_login_email"]]//div[@role="alert"]')
+      .should('have.text', '\'email\' is not a valid email')
+  })
 
-    it('Password input field is required', () => {
-        cy.visit('user/login')
-        cy.get('#normal_login_password')
-            .type('qwer123Ty')
-            .clear()
+  it('Password input field is required', () => {
+    cy.get('#normal_login_password')
+      .type('test')
+      .clear()
 
-        cy.xpath('//div[contains(@class, "ant-col")][div//input[@id="normal_login_password"]]//div[@role="alert"]')
-            .should('have.text', 'Required')
-    })
+    cy.xpath('//div[contains(@class, "ant-col")][div//input[@id="normal_login_password"]]//div[@role="alert"]')
+      .should('have.text', 'Required')
+  })
 })
 
